22287fcb90466ab52aa54c285ee682d3e9233d8b
rtexelm <texelist@gmail.com>
2023-09-01 00:32:27 -0400

    Fix schedule page with coalesce operators

--- page-schedule.php
+++ page-schedule.php
@@ -60,21 +60,21 @@ get_header();
 					    <?php if (function_exists('get_field')): ?>
 
 						    <?php 
-							$first_name			= get_field('first_name');
+							$first_name			 = get_field('first_name');
 							$last_name 			= get_field('last_name');
-							$additional_names 	= get_field('additional_names');
+							$group_names 		= get_field('group_names');
 							$project_title		= get_field('project_title');
 							$friday_location 	= get_field('friday_location');
 
 							$f_start = get_field('friday_start');
 							$f_start_h = explode(":", $f_start)[0];
-							$f_start_temp = explode(":", $f_start)[1];
+							$f_start_temp = explode(":", $f_start)[1] ?? null;
 							$f_start_m = substr($f_start_temp,0,2);
 							$f_start_ap = substr($f_start_temp, -2);
 
 							$f_end = get_field('friday_end');
 							$f_end_h = explode(":", $f_end)[0];
-							$f_end_temp = explode(":", $f_end)[1];
+							$f_end_temp = explode(":", $f_end)[1] ?? null;
 							$f_end_m = substr($f_end_temp,0,2);
 							$f_end_ap = substr($f_end_temp, -2);
 
@@ -100,11 +100,11 @@ get_header();
 										<?php echo $f_start_final."&ndash;".$f_end_final ?>	
 									</p>
 									<div class="project">
-										<span class="h3 artist-title"><?php echo $first_name . " " . $last_name . " " . $additional_names ?></span>
+										<span class="h3 artist-title"><?php echo $first_name . " " . $last_name . " " . $group_names ?></span>
 										<p class="artist-title"><?php echo $project_title ?></p>
 									</div>
 									<p class="location"><?php echo $friday_location ?></p>
-									<a class="button secondary" title="view <?php echo $first_name . ' ' . $last_name . ' ' . $additional_names ?>" href="<?php echo the_permalink(); ?>">View project</a>
+									<a class="button secondary" title="view <?php echo $first_name . ' ' . $last_name . ' ' . $group_names ?>" href="<?php echo the_permalink(); ?>">View project</a>
 								</div>
 							<?php endif ?>
 
@@ -120,21 +120,21 @@ get_header();
 					    <?php if (function_exists('get_field')): ?>
 
 						    <?php 
-							$first_name			= get_field('first_name');
+							$first_name			 = get_field('first_name');
 							$last_name 			= get_field('last_name');
-							$additional_names 	= get_field('additional_names');
+							$group_names 		= get_field('group_names');
 							$project_title		= get_field('project_title');
 							$saturday_location 	= get_field('saturday_location');
 
 							$sat_start = get_field('saturday_start');
 							$sat_start_h = explode(":", $sat_start)[0];
-							$sat_start_temp = explode(":", $sat_start)[1];
+							$sat_start_temp = explode(":", $sat_start)[1] ?? null;
 							$sat_start_m = substr($sat_start_temp,0,2);
 							$sat_start_ap = substr($sat_start_temp, -2);
 
 							$sat_end = get_field('saturday_end');
 							$sat_end_h = explode(":", $sat_end)[0];
-							$sat_end_temp = explode(":", $sat_end)[1];
+							$sat_end_temp = explode(":", $sat_end)[1] ?? null;
 							$sat_end_m = substr($sat_end_temp,0,2);
 							$sat_end_ap = substr($sat_end_temp, -2);
 
@@ -159,11 +159,11 @@ get_header();
 										<?php echo $sat_start_final."&ndash;".$sat_end_final ?>	
 									</p>
 									<div class="project">
-										<span class="h3 artist-title"><?php echo $first_name . " " . $last_name . " " . $additional_names ?></span>
+										<span class="h3 artist-title"><?php echo $first_name . " " . $last_name . " " . $group_names ?></span>
 										<p class="artist-title"><?php echo $project_title ?></p>
 									</div>
 									<p class="location"><?php echo $saturday_location ?></p>
-									<a class="button secondary" title="view <?php echo $first_name . ' ' . $last_name . ' ' . $additional_names ?>" href="<?php echo the_permalink(); ?>">View project</a>
+									<a class="button secondary" title="view <?php echo $first_name . ' ' . $last_name . ' ' . $group_names ?>" href="<?php echo the_permalink(); ?>">View project</a>
 								</div>
 							<?php endif ?>
 
@@ -187,13 +187,13 @@ get_header();
 
 							$sun_start = get_field('sunday_start');
 							$sun_start_h = explode(":", $sun_start)[0];
-							$sun_start_temp = explode(":", $sun_start)[1];
+							$sun_start_temp = explode(":", $sun_start)[1] ?? null;
 							$sun_start_m = substr($sun_start_temp,0,2);
 							$sun_start_ap = substr($sun_start_temp, -2);
 
 							$sun_end = get_field('sunday_end');
 							$sun_end_h = explode(":", $sun_end)[0];
-							$sun_end_temp = explode(":", $sun_end)[1];
+							$sun_end_temp = explode(":", $sun_end)[1] ?? null;
 							$sun_end_m = substr($sun_end_temp,0,2);
 							$sun_end_ap = substr($sun_end_temp, -2);
 
