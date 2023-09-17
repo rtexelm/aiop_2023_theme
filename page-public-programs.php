18ce28a7058db0e4562eaccb13cc45ce2bb7c637
rtexelm <texelist@gmail.com>
2023-09-16 22:30:40 -0400

    Complete public programs mobile page

--- page-public-programs.php
+++ page-public-programs.php
@@ -20,33 +20,69 @@ get_header();
     <h1 class="h1">Public Programs</h1>
     <section class="programs">
         <div class="program-container" id="program-01">
-            <h3>Runway & Panel Discussion</h3>
-            <p class="p3 date">Friday, October 13th, 2023</p>
-            <p class="p2 location">Bureau of General Services, Queer Division at the LGBGQ Center on 13th Street.</p>
-            <p class="p2 time">7-8:30pm</p>
-            <p class="p4">Join us for a runway/panel presentation of some kind at the Bureau of General Services, Queer
-                Division at the LGBGQ Center on 13th Street.
+            <h3>Walk & Talk</h3>
+            <p>Join us for a runway/panel presentation of some kind at the Bureau of General Services, Queer Division at
+                the LGBGQ Center on 13th Street.
             </p>
+            <div class="date-time">
+                <div class="date">
+                    <h4>Date</h4>
+                    <p>Friday, October 13th, 2023</p>
+                </div>
+                <div class="location">
+                    <h4>Location</h4>
+                    <p>Bureau of General Services, Queer Division at the LGBGQ Center on 13th Street.</p>
+                </div>
+                <div class="time">
+                    <h4>Time</h4>
+                    <p>7-8:30pm</p>
+                </div>
+            </div>
             <a href="https://www.eventbrite.com/e/art-in-odd-places-2022story-video-virtual-evening-tickets-407219312927"
-                class="button inline">RSVP here</a>
+                class="button pink">RSVP</a>
         </div>
         <div class="program-container" id="program-02">
-            <h2>What&rsquo;s Your STORY?</h2>
-
-            <p class="p2">Friday, September 23, 2022, 6–7:30pm, Bureau of General Services: Queer Division / Room 210,
-                LGBTQIA+ Center, 208 W 13th Street</p>
-            <p class="p3"> A selection of festival artists telling their stories. Featuring Nick Daniels, Jana Greiner,
-                Juan Hernadez with Mai Tran, Vivek Sebastian, Heather Sincavage, and Yu-Ching Wang. Hosted by Jessica
-                Elaine Blinkhorn.</p>
+            <h3>Paper DRESS Ball</h3>
+            <p> Join us for our reception and paper dress ball inspired by the exhibition currently up at the MAD museum
+                also at the LGBTQ Center on 13th.
+            </p>
+            <div class="date-time">
+                <div class="date">
+                    <h4>Date</h4>
+                    <p>Friday, October 13th, 2023</p>
+                </div>
+                <div class="location">
+                    <h4>Location</h4>
+                    <p>Bureau of General Services, Queer Division at the LGBGQ Center on 13th Street.</p>
+                </div>
+                <div class="time">
+                    <h4>Time</h4>
+                    <p>7-8:30pm</p>
+                </div>
+            </div>
             <a href="https://www.eventbrite.com/e/art-in-odd-places-2022-story-presents-whats-your-story-tickets-408967050457"
-                class="button inline">RSVP here</a>
+                class="button pink">Tickets</a>
         </div>
         <div class="program-container" id="program-03">
-            <h2>Critical Mass</h2>
-            <p class="p2"> Saturday, September 24, 2022, 3-5pm, 14th Street between Sixth and Eighth Avenues.</p>
-            <p clas="p3">A selection of the artists' projects in a concentrated area to make the festival more
-                accessible for festival goers.</p>
-        </div>
+            <h3>Runway</h3>
+            <p>Come see our artists and walk the runway on 14th street for a couple hours.
+            </p>
+            <div class="date-time">
+                <div class="date">
+                    <h4>Date</h4>
+                    <p>Friday, October 13th, 2023</p>
+                </div>
+                <div class="location">
+                    <h4>Location</h4>
+                    <p>Bureau of General Services, Queer Division at the LGBGQ Center on 13th Street.</p>
+                </div>
+                <div class="time">
+                    <h4>Time</h4>
+                    <p>7-8:30pm</p>
+                </div>
+            </div>
+            <a href="https://www.eventbrite.com/e/art-in-odd-places-2022-story-presents-whats-your-story-tickets-408967050457"
+                class="button pink">RSVP</a>
     </section>
 </main><!-- #primary -->
 
